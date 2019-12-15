74279841f581c0890b61ca06b09696a940803030
oscarjc19 <oscar.jimenez.carmona@gmail.com>
2019-12-14 19:55:39 -0600

    Added validations

--- src/main/java/com/softcorpcr/idp/controllers/AuthorizationController.java
+++ src/main/java/com/softcorpcr/idp/controllers/AuthorizationController.java
@@ -1,17 +1,24 @@
 package com.softcorpcr.idp.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.softcorpcr.idp.model.InvalidTokenResponse;
 import com.softcorpcr.idp.model.Request;
 import com.softcorpcr.idp.model.Response;
 import com.softcorpcr.idp.security.JwtTokenUtil;
 import com.softcorpcr.idp.services.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("api")
@@ -27,8 +34,22 @@ public class AuthorizationController {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
-    @RequestMapping(value = "/authorize", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(String token) throws Exception {
-        return ResponseEntity.ok(jwtTokenUtil.validateToken(token));
+    @RequestMapping(value = "/authorize", method = RequestMethod.GET)
+    public ResponseEntity<?> createAuthenticationToken(String access_token) throws Exception {
+        try {
+            if (jwtTokenUtil.validateToken(access_token)) {
+                return ResponseEntity.ok(new User(jwtTokenUtil.getUsernameFromToken(access_token), "", new ArrayList<>()));
+            } else {
+                InvalidTokenResponse response = new InvalidTokenResponse();
+                response.setAccess_token(access_token);
+                response.setMessage("Invalid Token");
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, new ObjectMapper().writeValueAsString(response));
+            }
+        } catch (Exception e) {
+            InvalidTokenResponse response = new InvalidTokenResponse();
+            response.setAccess_token(access_token);
+            response.setMessage("Invalid Token");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, new ObjectMapper().writeValueAsString(response));
+        }
     }
 }
---
+++ src/main/java/com/softcorpcr/idp/model/InvalidTokenResponse.java
@@ -0,0 +1,22 @@
+package com.softcorpcr.idp.model;
+
+public class InvalidTokenResponse {
+    private String message;
+    private String access_token;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+}

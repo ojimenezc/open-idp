28ea3a64a1565a482b20512b13ed9166fbcf8c56
ojimenez <ojimenez@softcorp-cr.com>
2020-02-04 22:18:22 -0600

    added tokens table

---
+++ src/main/java/com/softcorpcr/idp/repositories/TokensRepository.java
@@ -0,0 +1,18 @@
+package com.softcorpcr.idp.repositories;
+
+
+import com.softcorpcr.idp.model.entities.TokensEntity;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Configuration
+@ComponentScan("com.softcorpcr.idp.repositories")
+@Repository
+public interface TokensRepository extends CrudRepository<TokensEntity, Integer> {
+    @Query(value = "SELECT *  FROM tokens WHERE client_credentials=?1 AND expiry_date>NOW() LIMIT 1", nativeQuery = true)
+    TokensEntity getCurrentToken(@Param("client_credentials") String clientCredentials);
+}
